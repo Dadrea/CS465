db27d433afd6e099ace0e2c68adfd8ef32239a2a
Dadrea <rm4982@gmail.com>
2024-10-06 13:22:00 -0400

    Module 4 Completed Baseline

--- app.js
+++ app.js
@@ -7,17 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
-const handlebars = require('hbs');
+var handlebars = require('hbs');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'hbs');
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
-
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
